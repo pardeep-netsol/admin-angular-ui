3cf4c51bb186cc37c361e847d737940be88fe26a
Pardeep Saini <pardeep.saini@netsolutionsindis.com>
2015-10-01 11:36:30 +0530

    add reset password view

--- app/scripts/controllers/confirmation.js
+++ app/scripts/controllers/confirmation.js
@@ -1,7 +1,15 @@
 angular.module('angularjsApp')
   .controller('ConfirmationCtrl', function ($scope, $routeParams, secureService, $location) {
-  	debugger
   	if($location.url() == "/confirm_email/"+$routeParams.Token ){
-		secureService.confirmEmail($routeParams.Token)  		
+			secureService.confirmEmail($routeParams.Token)  		
+  	}else if($location.url() == "/reset_password/"+$routeParams.ResetToken){
+  		$scope.user_password = {
+  			user:{reset_password_token: $routeParams.ResetToken,
+  						password: "",
+  						password_confirmation: ""}
+  		}
+  	}
+  	$scope.resetUserPassword = function(user){
+  		secureService.resetPassword(user)
   	}
   });
